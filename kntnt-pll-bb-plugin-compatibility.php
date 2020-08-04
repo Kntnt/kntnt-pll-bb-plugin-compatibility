48c16b1abc6c81e435c417975d4b93b6bab0f825
Thomas Barregren <thomas@kntnt.com>
2020-08-04 21:09:00 +0200

    Update and rename kntnt-bb-pll-compatibility.php to kntnt-pll-bb-plugin-compatibility.php

---
+++ kntnt-pll-bb-plugin-compatibility.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Polylang and Beaver Builder Page Builder compatibility plugin
+ * Plugin URI:        https://www.kntnt.com/
+ * Description:       Makes Polylang compatible with Beaver Builder Page Builder.
+ * Version:           1.1.0
+ * Author:            Thomas Barregren
+ * Author URI:        https://www.kntnt.com/
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+
+namespace Kntnt\PLL_BB_Plugin_Compatibility;
+
+defined( 'ABSPATH' ) && new Plugin;
+
+final class Plugin {
+
+    public function __construct() {
+        add_action( 'init', [ $this, 'run' ] );
+    }
+
+    public function run() {
+        if ( function_exists( 'pll_current_language' ) ) {
+            add_filter( 'fl_builder_loop_query_args', [ $this, 'fl_builder_loop_query_args' ] );
+            add_filter( 'rewrite_rules_array', [ $this, 'rewrite_rules_array' ] );
+
+        };
+    }
+
+    public function fl_builder_loop_query_args( $args ) {
+        $args['lang'] = pll_current_language();
+        return $args;
+    }
+
+    // Beaver builder add its rewrite rules to $wp_rewrite->extra_rules_top
+    // which isn't processed by PLL_Links_Directory->rewrite_rules(). This
+    // function extracts the  extra rules added to the top and process them
+    // the same way as Polylang does.
+    public function rewrite_rules_array( $rules ) {
+
+        global $wp_rewrite;
+        $newrules = [];
+
+        $languages = PLL()->model->get_languages_list( [ 'fields' => 'slug' ] );
+        if ( PLL()->options['hide_default'] ) {
+            $languages = array_diff( $languages, [ PLL()->options['default_lang'] ] );
+        }
+
+        if ( ! empty( $languages ) ) {
+            $slug = $wp_rewrite->root . ( PLL()->options['rewrite'] ? '' : 'language/' ) . '(' . implode( '|', $languages ) . ')/';
+        }
+
+        foreach ( $rules as $key => $rule ) {
+            $newrules[ $slug . str_replace( $wp_rewrite->root, '', ltrim( $key, '^' ) ) ] = str_replace(
+                [ '[8]', '[7]', '[6]', '[5]', '[4]', '[3]', '[2]', '[1]', '?' ],
+                [ '[9]', '[8]', '[7]', '[6]', '[5]', '[4]', '[3]', '[2]', '?lang=$matches[1]&' ],
+                $rule
+            );
+            $newrules[ $key ] = $rule;
+        }
+
+        return $newrules;
+
+    }
+
+}
